487d4916e950623f11e2c2fb7d7dc46e51c4832f
Aswin <aswindilip5@gmail.com>
2023-10-04 21:10:16 +0530

    Basic python program using count method

---
+++ S1/Python Lab/Python9.py
@@ -0,0 +1,7 @@
+#Accept a string from the user and count the occurenence of a particular character in the string
+String = input("Enter the String :")
+char = input("Enter the character :")
+
+NoOfChar = String.count(char)
+
+print("The number of char present in the String is ",NoOfChar)
