8895dad0a4ec7030e29fe86397dc9439f83ff916
Aswin <aswindilip5@gmail.com>
2023-10-04 20:47:47 +0530

    Basic python program to use max method

---
+++ S1/Python Lab/Python6.py
@@ -0,0 +1,7 @@
+#find the biggest of 3 numbers entered by user
+
+num1 = int(input("Enter first number :"))
+num2 = int(input("Enter second number :"))
+num3 = int(input("Enter third number :"))
+
+print("The Maximum of the three numbers is ",max(num1,num2,num3))
